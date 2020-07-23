0d3cebc018a78471b4e377997984ba99de68a1d4
willowy <vazquezjoshua47@gmail.com>
2020-07-23 18:27:21 -0500

    fix product y admincategory

--- src/views/Product.jsx
+++ src/views/Product.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from '../components/Header/Header'
-import Menu from '../components/Nav/Nav'
 import Footer from '../components/Footer/Footer'
+import Menu from '../components/Nav/Nav'
 import Container from 'react-bootstrap/Container'
 import Image from 'react-bootstrap/Image'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import producto from '../assets/img/producto_cuadro.png'
-import Media from 'react-bootstrap/Media'
+import "../assets/css/main.css"
 
+import Media from 'react-bootstrap/Media'
 const  Product  = () => {
     return ( 
         <>
@@ -16,28 +17,29 @@ const  Product  = () => {
     <Menu></Menu>
     <Container>
         <Row className="Row_colum">
-        <h2 className="color_product_tittle_gris product_tittle">Zapatillas Mujer</h2>
+        <h2 className="gris_product_tittle">Zapatillas Mujer</h2>
         <h2 className="color_product_tittle">/ Adidas Sport - Shakira</h2>
         </Row>
     </Container>
+
     <Media>
     <Container className="container_center">
         <Row xs={2} md={2} sm={2} className="containerP_margin">
-            <Col className="product_foto">
+            <Col className="margin_foto">
             <Image src={producto} rouded fluid/>
             </Col>
             <Media.Body>
             <Col >
-            <h3 className="product_name">Adidas Sport - Shakira</h3>
+            <h3 className="name_product">Adidas Sport - Shakira</h3>
             </Col>
             <Col>
-            <h3 className="product_precio">$/.25.00</h3>
+            <h3 className="precio_product">$/.25.00</h3>
             </Col>
             <Col> 
-            <p className="product_SKU">SKU: 76sa6</p>
+            <p className="SKU_product">SKU: 76sa6</p>
             </Col>
             <Col>
-            <p className="product_descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p className="descripcion_product">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </Col>
             <Container className="button_center">
                 <button className="product_button_menos"> <p className="product_button_iconmenos" >-</p> </button>
@@ -47,7 +49,6 @@ const  Product  = () => {
             <Container className="button_center2">
                 <button className="button_addcart"> <p className="product_button_addcart_text">Agregar a Carrito</p> </button>
             </Container>
-
             </Media.Body>
         </Row>
     </Container>
